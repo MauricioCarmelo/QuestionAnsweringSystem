abeaa0511a57d88e0ec010b8da05e588f2911a7d
eduardogc8 <eduardogcortes8@gmail.com>
2021-04-18 11:46:11 -0300

    Support for SQuAD1.1 dataset ready [feature]

--- src/datasetreader/DatasetReaderSQuAD11.py
+++ src/datasetreader/DatasetReaderSQuAD11.py
@@ -8,59 +8,73 @@ class DatasetReaderSQuAD11(DatasetReader):
     def load_entries(self):
 
         try:
-            f = open(self.path + 'dev-v1.1.json')
-            data = json.load(f)
-
-            """
-                        json structure:
-
-                            version
-                            data (list)
-                                title
-                                paragraphs (list)
-                                    qas (list)
-                                        question
-                                        id
-                                        is_impossible
-                                        context
-                                        answers (list)
-                                            text
-                                            answer_start
-                                        plausible_answers (list)
-                                            text
-                                            answer_start
-            """
-
-            # contexts = {}  # ToDo: in the future, load the contexts as a dataset resource (issue *)
-
             read_entries = []
-            for d in data["data"]:
-                for paragraph in d["paragraphs"]:
-                    context = paragraph["context"]
-                    # context_id = len(contexts)  # (issue *)
-                    # contexts[context_id] = context  # (issue *)
-                    for question in paragraph["qas"]:
-                        entry = {}
-                        entry["question"] = question["question"]
-                        # entry["is_impossible"] = question["is_impossible"]
-                        _answers = []
-                        for answer in question["answers"]:
-                            _answer = {"answer": answer["text"],
-                                       "contexts": {"context": context,  # ToDo: use context ID (issue *)
-                                                    "start_index_answer": answer["answer_start"]},
-                                      }
-                            _answers.append(_answer)
-                        if "plausible_answers" in question:
-                            for answer in question["plausible_answers"]:
-                                _answer = {"answer": answer["text"],
-                                           "contexts": {"context": context,  # ToDo: use context ID (issue *)
-                                                        "start_index_answer": answer["answer_start"]},
-                                          }
-                                _answers.append(_answer)
-                        entry["answers"] = _answers
-                        read_entries.append(entry)
+
+            # train-v1.1.json file
+            f = open(self.path + 'train-v1.1.json')
+            read_entries += self.__read_squad_json_format(f, "train")
+
+            # dev-v1.1.json file
+            f = open(self.path + 'dev-v1.1.json')
+            read_entries += self.__read_squad_json_format(f, "dev")
+
             return read_entries
 
         except Exception as e:
             logging.error(str(e))
             return None
+
+    def __read_squad_json_format(self, file, pre_evaluation_group):
+        """
+        It read a json file SQuAD format and return its resource entries.
+
+        json SQuAD file structure:
+            version
+            data (list)
+                title
+                paragraphs (list)
+                    qas (list)
+                        question
+                        id
+                        is_impossible
+                        context
+                        answers (list)
+                            text
+                            answer_start
+                        plausible_answers (list)
+                            text
+                            answer_start
+        """
+        data = json.load(file)
+
+        # contexts = {}  # ToDo: in the future, load the contexts as a dataset resource (issue *)
+
+        read_entries = []
+        for d in data["data"]:
+            for paragraph in d["paragraphs"]:
+                context = paragraph["context"]
+                # context_id = len(contexts)  # (issue *)
+                # contexts[context_id] = context  # (issue *)
+                for question in paragraph["qas"]:
+                    entry = {}
+                    entry["question"] = question["question"]
+                    if "is_impossible" in question:
+                        entry["is_impossible"] = question["is_impossible"]
+                    _answers = []
+                    for answer in question["answers"]:
+                        _answer = {"answer": answer["text"],
+                                   "contexts": {"context": context,  # ToDo: use context ID (issue *)
+                                                "start_index_answer": answer["answer_start"]},
+                                   }
+                        _answers.append(_answer)
+                    if "plausible_answers" in question:
+                        for answer in question["plausible_answers"]:
+                            _answer = {"answer": answer["text"],
+                                       "contexts": {"context": context,  # ToDo: use context ID (issue *)
+                                                    "start_index_answer": answer["answer_start"]},
+                                       }
+                            _answers.append(_answer)
+                    entry["answers"] = _answers
+                    entry["pre_evaluation_group"] = pre_evaluation_group
+                    read_entries.append(entry)
+        return read_entries
