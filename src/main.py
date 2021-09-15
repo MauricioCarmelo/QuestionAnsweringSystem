d4f9567857c502e5b273b466ccee242176deee28
MauricioCarmelo <mauriciocarmelo2@gmail.com>
2021-09-15 07:02:09 -0300

    Remove unused imports

--- src/main.py
+++ src/main.py
@@ -1,10 +1,5 @@
 from src.Simulation import Simulation
-from src.tasks.TaskGenerateQuery import TaskGenerateQuery
-from src.Pipeline import Pipeline
 import logging
-import json
-from Resource import Resource
-from src.datasetreader.ImplementedDatasetReaders import ImplementedDatasetReaders
 
 logging.basicConfig(level=logging.DEBUG, filename='./log/my_logs.log')
 
